b6b1c98532c05884294b2d70d5e2550c0ab73e44
acentfrio <acentfrio@gmail.com>
2015-10-11 20:51:59 +0100

    "header and some items added on Navigation Bar"

--- app/src/main/java/com/example/alex/tmdbapp/MainActivity.java
+++ app/src/main/java/com/example/alex/tmdbapp/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.alex.tmdbapp;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends LoggingActivity {
+    //private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-
+    private static final int LAYOUT = R.layout.activity_main;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
+        initNavigationBar();
 
+    }
+
+    private void initNavigationBar() {
+        //drawerLayout = (DrawerLayout)findViewById(R.id.navigation);
     }
 
     private void initToolbar() {
